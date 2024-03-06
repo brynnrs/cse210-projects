123d8012818fb7795abd6c850287d4013c0e2ad4
brynnrs <brynn@byui.edu>
2024-03-06 01:51:30 -0700

    Completed Core Requirement 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int gradePercentage = int.Parse(answer);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("Letter Grade: A");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("Letter Grade: B");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Letter Grade: C");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("Letter Grade: D");
+        }
+        else
+        {
+            Console.WriteLine("Letter Grade: F");
+        }
     }
 }
