57f38da5e488056c1a4055431940cf8d6e5f56cc
brynnrs <brynn@byui.edu>
2024-03-08 02:35:44 -0700

    Completed Core Requirement 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
-
+using System.Diagnostics.CodeAnalysis; //.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        int inputNumber = -1;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            inputNumber = int.Parse(Console.ReadLine());
+            
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+            
+        }
+        while (inputNumber != 0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
     }
 }
