06c8962ee9cd397ef8a2ca157ac9120683044d63
brynnrs <brynn@byui.edu>
2024-03-08 01:49:16 -0700

    Completed Core Requirement 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,22 +7,27 @@ class Program
         Console.Write("What is the magic number? ");
         string inputNumber = Console.ReadLine();
         int magicNumber = int.Parse(inputNumber);
+        
+        int guessNumber = -1;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string inputNumber2 = Console.ReadLine();
+            guessNumber = int.Parse(inputNumber2);
 
-        Console.Write("What is your guess? ");
-        string inputNumber2 = Console.ReadLine();
-        int guessNumber = int.Parse(inputNumber2);
-
-        if (guessNumber < magicNumber)
-        {
-            Console.Write("Higher");
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
-        else if (guessNumber > magicNumber)
-        {
-            Console.Write("Lower");
-        }
-        else
-        {
-            Console.Write("You guessed it!");
-        }
+        while (guessNumber != magicNumber);
     }
 } 
