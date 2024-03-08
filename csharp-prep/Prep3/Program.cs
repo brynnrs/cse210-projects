2b9abde560e3d3d8158dabd771eb4095df4cf5f3
brynnrs <brynn@byui.edu>
2024-03-07 23:27:27 -0700

    Completed Core Requirement 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string inputNumber = Console.ReadLine();
+        int magicNumber = int.Parse(inputNumber);
+
+        Console.Write("What is your guess? ");
+        string inputNumber2 = Console.ReadLine();
+        int guessNumber = int.Parse(inputNumber2);
+
+        if (guessNumber < magicNumber)
+        {
+            Console.Write("Higher");
+        }
+        else if (guessNumber > magicNumber)
+        {
+            Console.Write("Lower");
+        }
+        else
+        {
+            Console.Write("You guessed it!");
+        }
     }
-}
+} 
