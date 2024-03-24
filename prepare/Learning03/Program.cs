d559a5dd68e1c6914df770a16b2884a9c31ac050
brynnrs <brynn@byui.edu>
2024-03-24 02:50:37 -0600

    Created and Tested Constructors

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6,7);
         Console.WriteLine("Hello Learning03 World!");
     }
 }
