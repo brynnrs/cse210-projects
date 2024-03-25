e239ea2250baeea5793a524b47809ce49193aa91
brynnrs <brynn@byui.edu>
2024-03-25 13:09:58 -0600

    Deleted unnecessary variables

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,26 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction f1 = new Fraction();
-        Fraction f2 = new Fraction(6);
-        Fraction f3 = new Fraction(6,7);
+        Fraction f1 = new Fraction(1);
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3,4);
+        Fraction f4 = new Fraction(1,3);
 
-        Fraction f4 = new Fraction(1);
-        Fraction f5 = new Fraction(5);
-        Fraction f6 = new Fraction(3,4);
-        Fraction f7 = new Fraction(1,3);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
 
         Console.WriteLine(f4.GetFractionString());
         Console.WriteLine(f4.GetDecimalValue());
-
-        Console.WriteLine(f5.GetFractionString());
-        Console.WriteLine(f5.GetDecimalValue());
-
-        Console.WriteLine(f6.GetFractionString());
-        Console.WriteLine(f6.GetDecimalValue());
-
-        Console.WriteLine(f7.GetFractionString());
-        Console.WriteLine(f7.GetDecimalValue());
 
     }
 }
