b70e4410a68f0a82d35db73d5ba7ff06d52298f6
brynnrs <brynn@byui.edu>
2024-03-28 10:43:35 -0600

    Wrote Word Methods

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -5,26 +5,37 @@ public class Word
 
     public Word(string text)
     {
-
+        _text = text;
+        _isHidden = false;
     }
 
     public void Hide()
     {
-        
+        _isHidden = true;
     }
     
     public void Show()
     {
-        
+        _isHidden = false;
     }
 
     public bool IsHidden()
     {
-        return false;
+        if (_isHidden == true)
+            return true;
+        else
+            return false;
     }
 
     public string GetDisplayText()
     {
-        return "";
+        string underscores = "";
+
+        if (_isHidden == false)
+            return _text;
+        else
+            for (int i = 0; i <_text.Length; i++)
+                underscores += "_";
+            return underscores;
     }
 }
