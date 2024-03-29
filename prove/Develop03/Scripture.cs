0863ba2c04cfc00153db07f6ec9364f427cebece
brynnrs <brynn@byui.edu>
2024-03-29 00:36:00 -0600

    Wrote HideRandomWords Method

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -4,6 +4,7 @@ public class Scripture
 {
     private Reference _reference;
     private List<Word> _words;
+    private List<int> numberDeck; 
 
     public Scripture(Reference Reference, string text)
     {
@@ -16,12 +17,33 @@ public class Scripture
         {
             Word aWord = new Word(w);
             _words.Add(aWord);
-        } 
+        }
+        numberDeck = new List<int>();
+        for (int i = 0; i < _words.Count; i++)
+        {
+            numberDeck.Add(i);
+        }
     }
 
     public void HideRandomWords(int numberToHide)
     {
+        if(numberToHide >= numberDeck.Count){
+            for(int i = 0; i< _words.Count; i++)
+            {
+                _words[i].Hide();
+            }
+        }
+        else{
+            Random randomGenerator = new Random();
 
+            for (int i = 0; i < numberToHide; i++)
+            {
+                int wordToHideIndex = numberDeck[randomGenerator.Next(numberDeck.Count)];
+                numberDeck.Remove(wordToHideIndex);
+                _words[wordToHideIndex].Hide();
+            }
+
+        }
     }
 
     public string GetDisplayText()
