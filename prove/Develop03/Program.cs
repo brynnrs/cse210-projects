b741a32deb23be715f203cf71670051a8bca5263
brynnrs <brynn@byui.edu>
2024-03-28 23:44:50 -0600

    Wrote Main Program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        bool isRunning = true;
+        
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+        
+        while (isRunning)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press enter to continue or type 'quit' to finish");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "quit" || scripture.IsCompletelyHidden())
+            {
+                return;
+            }
+            scripture.HideRandomWords(3);
+        }
+        
     }
 }
