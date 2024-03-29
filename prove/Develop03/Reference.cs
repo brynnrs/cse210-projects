83be59a8ba2b38e8d389328c89243040e9112e1f
brynnrs <brynn@byui.edu>
2024-03-28 23:05:47 -0600

    Added forgotten curly braces

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -24,8 +24,12 @@ public class Reference
     public string GetDisplayText()
     {
         if (_endVerse == 0)
+        {
             return $"{_book} {_chapter}:{_verse}";
+        }
         else
+        {
             return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
     }
 }
