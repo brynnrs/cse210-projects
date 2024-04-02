5b61233fe3fe390c4d40b6f2014d1d0c50c7875f
brynnrs <brynn@byui.edu>
2024-04-02 03:45:43 -0600

    Completed Step 5

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        MathAssignment mathAssign = new MathAssignment("Roberto Rodriguez", "Fractions","7.3","8-19");
-        Console.WriteLine(mathAssign.GetSummary());
-        Console.WriteLine(mathAssign.GetHomeworkList());        
+        WritingAssignment writingAssign = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssign.GetSummary());
+        Console.WriteLine(writingAssign.GetWritingInformation());        
     }
 }
