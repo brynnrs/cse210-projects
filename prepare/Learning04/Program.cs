eab7a67bc6d082c9cfeb894600c8faee956538e7
brynnrs <brynn@byui.edu>
2024-04-02 03:49:24 -0600

    Added back test code for classes "Assingment" and "MathAssignment"

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        Assignment simpleAssign = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simpleAssign.GetSummary());
+
+        MathAssignment mathAssign = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssign.GetSummary());
+        Console.WriteLine(mathAssign.GetHomeworkList());
+
         WritingAssignment writingAssign = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
         Console.WriteLine(writingAssign.GetSummary());
         Console.WriteLine(writingAssign.GetWritingInformation());        
