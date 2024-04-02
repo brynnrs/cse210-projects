3aec0c4c3f37b611d2e95de3640f50dcf1dca409
brynnrs <brynn@byui.edu>
2024-04-02 03:03:51 -0600

    Completed Steps 1, 2, and 3

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment simpleAssign = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simpleAssign.GetSummary());
     }
 }
