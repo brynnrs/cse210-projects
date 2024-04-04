2638a6dd607d941953ead36d977421cbd5b688c2
brynnrs <brynn@byui.edu>
2024-04-04 12:35:16 -0600

    Wrote Menu Loop

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int response = 0;
+
+        while (response !=4)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start breathing activity");
+            Console.WriteLine("2. Start reflecting activity");
+            Console.WriteLine("3. Start listing activity");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine("Select a choice from the menu:");
+            response = int.Parse(Console.ReadLine());
+
+            if (response == 1)
+            {
+                response = 0;
+            }
+
+            else if (response == 2)
+            {
+                response = 0;
+            }
+
+            else if (response == 3)
+            {
+                response = 0;
+            }
+
+            else
+            {
+                Console.WriteLine("Please choose a number corresponding to the menu (1-4)");
+            }
+        }
     }
 }
