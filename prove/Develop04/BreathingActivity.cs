64ef332a4d09aaf372c4796b9b372b613ce7b6e6
brynnrs <brynn@byui.edu>
2024-04-04 17:51:17 -0600

    Wrote BreathingActivity methods

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,12 +1,22 @@
-public class BreathingActivity
+public class BreathingActivity : Activity
 {
-    public BreathingActivity()
+    public BreathingActivity() : base()
     {
-
+        _name = "Breathing Activity";
+        _description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
     }
 
     public void Run()
     {
-        
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine($"Breathe in...");
+            ShowCountDown(_pause);
+            Console.WriteLine($"Breathe out...");
+            ShowCountDown(_pause);
+        }
     }
 }
