a84c22b31e6decc404cd52bb45ae955e260bdbed
brynnrs <brynn@byui.edu>
2024-04-09 21:34:40 -0600

    Wrote Reception Class

---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,18 @@
+public class Reception : Event
+{
+    private string _rsvpEmail;
+
+    public Reception(string rsvpEmail, string eventTitle, 
+        string description, 
+        string date, 
+        string time, 
+        Address address
+        ) : base (eventTitle, description, date, time, address, "Reception")
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+    public string GetFullDetails()
+    {
+        return $"Event Type: {_eventType}\n{GetStandardDetails()}\nRSVP: {_rsvpEmail}";
+    }
+}
