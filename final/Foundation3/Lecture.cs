f99273dc96685ff474db1f5580b104b16670e455
brynnrs <brynn@byui.edu>
2024-04-09 21:33:54 -0600

    Wrote Lecture Class

---
+++ final/Foundation3/Lecture.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+
+public class Lecture : Event
+{
+    private string _speaker;
+    private int _capacityLimit;
+    public Lecture (
+        string speaker, 
+        int capacityLimit, 
+        string eventTitle, 
+        string description, 
+        string date, 
+        string time, 
+        Address address
+        ) : base (eventTitle, description, date, time, address, "Lecture")
+    {
+        _speaker = speaker;
+        _capacityLimit = capacityLimit;
+    }
+    
+    public string GetFullDetails()
+    {
+        return $"Event Type: {_eventType}\n{GetStandardDetails()}\nSpeaker: {_speaker}\nCapacity Limit: {_capacityLimit}";
+    }
+}
