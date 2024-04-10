aa28d95b57686d515941a2eb885692c4c6a11244
brynnrs <brynn@byui.edu>
2024-04-10 14:39:12 -0600

    Wrote main Program

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        DateOnly date = DateOnly.FromDateTime(DateTime.Today);
+
+        Running running = new Running(date, 25, 3);
+
+        Cycling cycling = new Cycling(date, 30, 15);
+
+        Swimming swimming = new Swimming(date, 30, 20);
+
+        List<Activity> activities = new List<Activity>
+        {
+            running,
+            cycling,
+            swimming
+        };
+
+        Console.Clear();
+        foreach (Activity a in activities)
+        {
+            Console.WriteLine($"- {a.GetSummary()}");
+            Console.WriteLine();
+        }
     }
 }
