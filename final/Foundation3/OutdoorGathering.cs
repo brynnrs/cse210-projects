a9bcd2985f72fbc3eb7dc9e500944321601f8a6e
brynnrs <brynn@byui.edu>
2024-04-09 21:34:18 -0600

    Wrote OutdoorGathering Class

---
+++ final/Foundation3/OutdoorGathering.cs
@@ -0,0 +1,21 @@
+public class OutdoorGathering : Event
+{
+    private string _weather;
+
+    public OutdoorGathering(
+        string weather,
+        string eventTitle, 
+        string description, 
+        string date, 
+        string time, 
+        Address address
+        ) : base (eventTitle, description, date, time, address, "Outdoor Gathering")
+        {
+            _weather = weather;
+        }
+    public string GetFullDetails()
+    {
+        return $"Event Type: {_eventType}\n{GetStandardDetails()}\nWeather Forecast: {_weather}";
+    }
+  
+}
