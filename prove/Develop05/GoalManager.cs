f588febe90f60c61e0d811e17b82f90a76e45649
brynnrs <brynn@byui.edu>
2024-04-10 22:53:05 -0600

    wrote menu loop

--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -10,7 +10,46 @@ public class GoalManager
 
     public void Start()
     {
+        bool exit = false;
 
+            while (!exit)
+            {
+                Console.WriteLine("Menu Options: \n1. Create New Goal\n2. List Goals\n3. Save Goals\n4. Load Goals\n5. Record Event\n6. Quit\nSelect a choice from the menu: ");
+                int response = int.Parse(Console.ReadLine());
+
+            if (response == 1)
+            {
+               CreateGoal();
+            }
+
+            else if (response == 2)
+            {
+                ListGoalNames();
+                ListGoalDetails();
+            }
+
+            else if (response == 3)
+            {
+                SaveGoals();  
+            }
+
+            else if (response == 4)
+            {
+                LoadGoals();
+            }
+            else if (response == 5)
+            {
+                RecordEvent();
+            }
+            else if (response == 6)
+            {
+                exit = true;
+            }
+            else
+            {
+                Console.WriteLine("Please choose an option from the menu (1-6).");
+            }
+            }
     }    
 
     public void DisplayPlayerInfo()
@@ -38,13 +77,13 @@ public class GoalManager
 
     }
 
-    public void SaveGoal()
+    public void SaveGoals()
     {
 
     }
 
-    public void LoadGoal()
+    public void LoadGoals()
     {
-        
+
     }
 }
